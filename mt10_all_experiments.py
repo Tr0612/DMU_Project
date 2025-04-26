f0037404441e49a28afb40a105ac9da8810ac9a8
Chris Guthrie <guthriec93@gmail.com>
2025-04-26 17:51:03 -0600

    Add option to exclude overall multitask from experiment

--- mt10_all_experiments.py
+++ mt10_all_experiments.py
@@ -3,13 +3,19 @@ import metaworld
 
 
 def run_experiment(
-    total_steps, batch_size, include_individual_metalearn, include_overall_metalearn
+    total_steps,
+    batch_size,
+    include_individual_metalearn,
+    include_overall_multitask,
+    include_overall_metalearn,
 ):
     total_steps = int(float(total_steps))
     mt10 = metaworld.MT10()
     all_test_classes = list(mt10.train_classes.keys())
     mt1_params = benchmark_evaluator.TrainingParameters(
-        total_steps // 10, batch_size, None,
+        total_steps // 10,
+        batch_size,
+        None,
     )
     for test_class in all_test_classes:
         mt1 = metaworld.MT1(test_class)
@@ -50,15 +56,18 @@ def run_experiment(
                     ]
                 ),
             )
-    print("Evaluating MT10")
-    mt10_params = benchmark_evaluator.TrainingParameters(total_steps, batch_size, None)
-    benchmark_evaluator.evaluate_benchmark(
-        mt10,
-        False,
-        mt10_params,
-        100,
-        "_".join(["sac", "mt10", "default", str(total_steps), str(batch_size)]),
-    )
+    if include_overall_multitask:
+        print("Evaluating MT10")
+        mt10_params = benchmark_evaluator.TrainingParameters(
+            total_steps, batch_size, None
+        )
+        benchmark_evaluator.evaluate_benchmark(
+            mt10,
+            False,
+            mt10_params,
+            100,
+            "_".join(["sac", "mt10", "default", str(total_steps), str(batch_size)]),
+        )
     if include_overall_metalearn:
         print("Evaluating ML10")
         ml10 = metaworld.ML10()
@@ -67,5 +76,7 @@ def run_experiment(
             True,
             mt10_params,
             100,
-            "_".join(["sac", "metalearn10", "default", str(total_steps), str(batch_size)]),
+            "_".join(
+                ["sac", "metalearn10", "default", str(total_steps), str(batch_size)]
+            ),
         )
