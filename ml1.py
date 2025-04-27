e4bb19a20855e6e3d73bb363c2e97b9e2702ff7d
Chris Guthrie <guthriec93@gmail.com>
2025-04-26 20:17:44 -0600

    Update ML1 params

--- ml1.py
+++ ml1.py
@@ -1,12 +1,12 @@
 import benchmark_evaluator
 import metaworld
 
-reach_ml1 = metaworld.ML1("reach-v2")
+reach_ml1 = metaworld.ML1("window-close-v2")
 
 model, evaluation = benchmark_evaluator.evaluate_benchmark(
     reach_ml1,
     True,
-    benchmark_evaluator.TrainingParameters(1e4, 32, None),
+    benchmark_evaluator.TrainingParameters(1e4, 32, None, [400, 400]),
     200,
     None,
 )
